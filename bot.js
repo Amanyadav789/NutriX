96e9dea8f4e58a6249dc8c2568346d3ecf9fa357
Amanyadav789 <yadavaman8511005211@gmail.com>
2024-10-17 01:03:42 +0530

    first commit

---
+++ bot.js
@@ -0,0 +1,10 @@
+
+window.embeddedChatbotConfig = {
+chatbotId: "4EANWV2jlCoIDQRtklgWJ",
+domain: "www.chatbase.co"
+}
+
+src="https://www.chatbase.co/embed.min.js"
+chatbotId="4EANWV2jlCoIDQRtklgWJ"
+domain="www.chatbase.co"
+defer
